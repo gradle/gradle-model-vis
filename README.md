54d526242262b6f8ba3f645b991042c572265770
Lóránt Pintér <lorant@gradle.com>
2016-02-01 23:25:08 +0100

    Added log processor

---
+++ README.md
@@ -0,0 +1,19 @@
+## Gradle Software Model Log Visualizer
+
+Here's how to use:
+
+1. Run Gradle with some task and convert the debug output to a JSON
+
+    ```text
+    $ ./gradlew --debug model | groovy process-log.groovy > samples/gradle-model.json
+    ```
+
+2. Start web-server
+
+    ```text
+    $ python -m SimpleHTTPServer 8080
+    ```
+
+3. Open [http://localhost:8080/#samples/gradle-model.json](http://localhost:8080/#samples/gradle-model.json)
+
+!["./gradlew model" visualized](images/model-visualization.png)
---
+++ process-log.groovy
@@ -0,0 +1,25 @@
+import groovy.json.JsonOutput
+
+def transition = ~/Transitioning model element '(.+?)' to state ([a-zA-Z]+)/
+def registration = ~/"Registering model element '(.+?)'"/
+def data = [[path:'', type: 'state-changed', state: 'Created']]
+System.in.eachLine { line ->
+    def match = transition.matcher(line)
+    if (match) {
+        data << [
+                path: match[0][1],
+                type: 'state-changed',
+                state: match[0][2]
+        ]
+    } else {
+        match = registration.matcher(line)
+        if (match) {
+            data << [
+                    path: match[0][1],
+                    type: 'state-changed',
+                    state: 'Registered'
+            ]
+        }
+    }
+}
+println JsonOutput.prettyPrint(JsonOutput.toJson(data))
