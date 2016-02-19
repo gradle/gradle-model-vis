bad32fd5d874134736204eb685c4304fc363b02b
Lóránt Pintér <lorant@gradle.com>
2016-02-20 00:03:10 +0100

    Introduce Angular-bound controls

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var init = function() {
+(function () {
+    var app = angular.module("vis", ["vis.tree"]);
+
     var width = 1200,
         height = 1000;
 
@@ -42,136 +44,129 @@ var init = function() {
     legend.exit().remove();
 
     var dataFile = location.hash ? location.hash.substring(1) : "samples/gradle-model.json";
-    console.log("Loading data file", dataFile);
 
-    d3.json(dataFile, function(error, events) {
-        if (error) throw error;
-
-        var layout = new TreeLayout(svg, d3, width, height);
-
-        var commands = processEvents(events, layout);
-        var time = 0;
-
-        $("#time").attr({
-            min: 0,
-            max: events.length
-        }).val(0);
-        $("#back").click(function() {
-            var value = parseInt($("#time").val());
-            value = Math.max(0, value - 1);
-            $("#time").val(value);
-            $("#time").trigger("input");
-        });
-        $("#forward").click(function() {
-            var value = parseInt($("#time").val());
-            value = Math.min(events.length, value + 1);
-            $("#time").val(value);
-            $("#time").trigger("input");
-        });
-        var playing = false;
-        var $playPause = $("#play-pause");
-        $playPause.click(function() {
-            var stop = function() {
-                playing = false;
-                $playPause.text(">>");
-            };
-            if (!playing) {
-                var playLoop = function() {
-                    var $time = $("#time");
-                    var cur = parseInt($time.val());
-                    var max = parseInt($time.attr('max'));
-                    if (cur < max) {
-                        $("#forward").trigger("click");
-                        if (playing) setTimeout(playLoop, 200);
+    app.factory("dataLoader", function ($q) {
+        return function (dataFile) {
+            console.log("Loading data file", dataFile);
+			return $q(function (resolve, reject) {
+                d3.json(dataFile, function(error, events) {
+                    if (error) {
+                        reject(error);
                     } else {
-                        stop();
+                        console.log("Loaded data file", dataFile);
+                        resolve(events);
                     }
+                });
+			});
+        };
+    });
+
+    app.controller("TimeController", function($scope, $timeout, dataLoader, LayoutFactory) {
+        $scope.time = 0;
+        $scope.commands = [];
+        $scope.playing = false;
+        $scope.stepTime = function (delta) {
+            var time = parseInt($scope.time) + delta;
+            if (time >= 0 && time < $scope.commands.length) {
+                $scope.time = time;
+                $scope.$digest();
+            }
+        };
+        dataLoader(dataFile).then(function (events) {
+            var layout = new LayoutFactory(svg, d3, width, height);
+            $scope.commands = processEvents(events, layout);
+            $scope.$watch("time", function (targetTime, time) {
+                redrawUntil(parseInt(targetTime), parseInt(time));
+            });
+            $scope.$watch("playing", function (playing) {
+                if (playing) {
+                    var playLoop = function() {
+                        if ($scope.playing && $scope.time < $scope.commands.length - 1) {
+                            $scope.time++;
+                            $timeout(playLoop, 200);
+                        } else {
+                            $scope.playing = false;
+                        }
+                        $scope.$digest();
+                    };
+                    $timeout(playLoop, 0);
+                }
+            });
+
+            var redrawUntil = function(targetTime, time) {
+                if (time < targetTime) {
+                    while (time < targetTime) {
+                        $scope.commands[time].forward();
+                        time++;
+                    }
+                } else {
+                    while (time > targetTime) {
+                        time--;
+                        $scope.commands[time].backward();
+                    }
+                }
+                layout.repaint();
+            }
+        });
+    });
+
+    var processEvents = function (events, layout) {
+        var existingNodes = {};
+        var commands = [];
+        var multiproject = Object.size(events.groupBy(function (e) { return e.project; })) > 1;
+        var normalizeEvent = function(event) {
+            if (!multiproject) {
+                return;
+            }
+            if (event.project != '') {
+               var path = '<ROOT '+event.project+'>';
+               if (event.path != '') { path += '.' + event.path; }
+               event.path = path;
+            }
+        }
+        var processEvent = function(event) {
+            normalizeEvent(event);
+            var existingNode = existingNodes[event.path];
+            if (existingNode) {
+                var nextState = event.state;
+                var previousState = existingNode.state;
+                commands.push({
+                    forward: function() {
+                        console.log("-> " + event.path + ": " + previousState + " -> " + nextState);
+                        layout.setState(event.path, nextState);
+                    },
+                    backward: function() {
+                        console.log("<- " + event.path + ": " + nextState + " -> " + previousState);
+                        layout.setState(event.path, previousState);
+                    }
+                });
+                existingNode.state = event.state;
+            } else {
+                commands.push({
+                    forward: function() {
+                        console.log("++ " + event.path);
+                        layout.addNode(event.path);
+                    },
+                    backward: function() {
+                        console.log("-- " + event.path);
+                        layout.removeNode(event.path);
+                    }
+                });
+                existingNodes[event.path] = {
+                    path: event.path,
+                    state: event.state
                 };
-                playing = true;
-                $playPause.text("||");
-                playLoop();
-            } else {
-                stop();
+            }
+        };
+        if (multiproject) {
+        processEvent({ "project": '', "path": '', state: 'Registered'});
+        events.forEach(function (event) {
+            if (!existingNodes['<ROOT '+event.project+'>']) {
+                  processEvent({ project:'', state: 'Registered', path: '<ROOT '+event.project+'>' });
             }
         });
-
-        var redrawUntil = function(targetTime) {
-            if (time < targetTime) {
-                while (time < targetTime) {
-                    commands[time].forward();
-                    time++;
-                }
-            } else {
-                while (time > targetTime) {
-                    time--;
-                    commands[time].backward();
-                }
-            }
-            layout.repaint();
         }
-
-        $("#time").on("input", function() {
-            redrawUntil(parseInt($(this).val()));
-        });
-    });
-};
-
-var processEvents = function (events, layout) {
-    var existingNodes = {};
-    var commands = [];
-    var multiproject = Object.size(events.groupBy(function (e) { return e.project; })) > 1;
-    var normalizeEvent = function(event) {
-        if (!multiproject) {
-            return;
-        }
-        if (event.project != '') {
-           var path = '<ROOT '+event.project+'>';
-           if (event.path != '') { path += '.' + event.path; }
-           event.path = path;
-        }
-    }
-    var processEvent = function(event) {
-        normalizeEvent(event);
-        var existingNode = existingNodes[event.path];
-        if (existingNode) {
-            var nextState = event.state;
-            var previousState = existingNode.state;
-            commands.push({
-                forward: function() {
-                    console.log("-> " + event.path + ": " + previousState + " -> " + nextState);
-                    layout.setState(event.path, nextState);
-                },
-                backward: function() {
-                    console.log("<- " + event.path + ": " + nextState + " -> " + previousState);
-                    layout.setState(event.path, previousState);
-                }
-            });
-            existingNode.state = event.state;
-        } else {
-            commands.push({
-                forward: function() {
-                    console.log("++ " + event.path);
-                    layout.addNode(event.path);
-                },
-                backward: function() {
-                    console.log("-- " + event.path);
-                    layout.removeNode(event.path);
-                }
-            });
-            existingNodes[event.path] = {
-                path: event.path,
-                state: event.state
-            };
-        }
+        events.forEach(processEvent);
+        return commands;
     };
-    if (multiproject) {
-    processEvent({ "project": '', "path": '', state: 'Registered'});
-    events.forEach(function (event) {
-        if (!existingNodes['<ROOT '+event.project+'>']) {
-              processEvent({ project:'', state: 'Registered', path: '<ROOT '+event.project+'>' });
-        }
-    });
-    }
-    events.forEach(processEvent);
-    return commands;
-};
+})();
--- js/tree-layout.js
+++ js/tree-layout.js
@@ -1,154 +1,160 @@
-var TreeLayout = function(svg, d3, width, height) {
-    var createNode = function (name, parentPath) {
-        var children = [];
-        var node = {
-            name: name,
-            parent: parentPath,
-            state: "Registered",
-            addChild: function (childName) {
-                var path = parentPath ? parentPath + "." + name : name;
-                children.push(createNode(childName, path));
-                children.sort(function (a, b) { return a.name.localeCompare(b.name); });
-                node.children = children;
+(function () {
+    var module = angular.module("vis.tree", []);
+
+    module.factory("LayoutFactory", function () {
+        return function (svg, d3, width, height) {
+        var createNode = function (name, parentPath) {
+                var children = [];
+                var node = {
+                    name: name,
+                    parent: parentPath,
+                    state: "Registered",
+                    addChild: function (childName) {
+                        var path = parentPath ? parentPath + "." + name : name;
+                        children.push(createNode(childName, path));
+                        children.sort(function (a, b) { return a.name.localeCompare(b.name); });
+                        node.children = children;
+                    }
+                };
+                return node;
             }
-        };
-        return node;
-    }
 
-    var root = createNode("", null);
+            var root = createNode("", null);
 
-    var tree = d3.layout.tree()
-        .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / (a.depth + 1); });
+            var tree = d3.layout.tree()
+                .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / (a.depth + 1); });
 
-    var diagonal = d3.svg.diagonal.radial()
-        .projection(function(d) {
-            return [d.y, d.x / 180 * Math.PI];
-        });
+            var diagonal = d3.svg.diagonal.radial()
+                .projection(function(d) {
+                    return [d.y, d.x / 180 * Math.PI];
+                });
 
-    var canvas = svg
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            var canvas = svg
+                .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var findChildIndex = function(parent, name) {
-        if (!parent.children) {
-            return -1;
-        }
-        for (var i = 0; i < parent.children.length; i++) {
-            if (parent.children[i].name == name) {
-                return i;
-            }
-        };
-        return -1;
-    };
-    var findNodeInternal = function (path) {
-        var node = root;
-        if (path !== "") {
-            var parts = path.split(/\./);
-            while (parts.length > 0) {
-                var name = parts.shift();
-                var childIndex = findChildIndex(node, name);
-                if (childIndex === -1) {
-                    return null;
+            var findChildIndex = function(parent, name) {
+                if (!parent.children) {
+                    return -1;
                 }
-                node = node.children[childIndex];
-            }
-        }
-        return node;
-    };
-    var findNode = function (path) {
-        var node = findNodeInternal(path);
-        if (node === null) {
-            throw "Cannot find node " + path;
-        }
-        return node;
-    };
-
-    this.hasNode = function (path) {
-        return findNodeInternal(path) !== null;
-    }
-
-    // Add and remove elements on the graph object
-    this.addNode = function(path) {
-        if (!path) {
-            return;
-        }
-        var idx = path.lastIndexOf('.');
-        var parentPath = idx === -1 ? "" : path.substring(0, idx);
-        var name = idx === -1 ? path : path.substring(idx + 1);
-        var parent = findNode(parentPath);
-        parent.addChild(name);
-    };
-
-    this.removeNode = function(path) {
-        if (!path) {
-            return;
-        }
-        var idx = path.lastIndexOf('.');
-        var parentPath = idx === -1 ? "" : path.substring(0, idx);
-        var name = idx === -1 ? path : path.substring(idx + 1);
-        var parent = findNode(parentPath);
-        var childIndex = findChildIndex(parent, name);
-        if (childIndex !== -1) {
-            parent.children.splice(childIndex, 1);
-        }
-    };
-
-    this.setState = function(path, state) {
-        var node = findNode(path);
-        node.state = state;
-    };
-
-    var getMaxDepth = function (node, currentDepth) {
-        var depth = currentDepth;
-        if (node.children) {
-            node.children.forEach(function (child) {
-                depth = Math.max(depth, getMaxDepth(child, currentDepth + 1))
-            });
-        }
-        return depth;
-    }
-
-    this.repaint = function () {
-        var maxDepth = getMaxDepth(root, 0);
-        tree.size([360, maxDepth * 100]);
-        var svg = canvas;
-        svg.selectAll('.node').remove();
-        svg.selectAll('.link').remove();
-        var nodes = tree.nodes(root);
-        var links = tree.links(nodes);
-
-        var link = svg.selectAll(".link")
-            .data(links);
-        link
-            .enter()
-            .append("path")
-            .attr("class", "link")
-            .attr("d", diagonal);
-
-        var node = svg.selectAll(".node")
-            .data(nodes);
-        var nodeGroup = node
-            .enter()
-            .append("g")
-            .attr("class", "node")
-            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
-
-        nodeGroup.append("circle")
-            .attr("r", 6)
-            .attr("class", function(d) { return d.state; });
-
-        nodeGroup.append("text")
-            .attr("dy", ".31em")
-            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-            .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-            .text(function(d) {
-                var name = d.name;
-                var idx = name.lastIndexOf(".");
-                if (idx !== -1) {
-                    name = name.substring(idx + 1);
+                for (var i = 0; i < parent.children.length; i++) {
+                    if (parent.children[i].name == name) {
+                        return i;
+                    }
+                };
+                return -1;
+            };
+            var findNodeInternal = function (path) {
+                var node = root;
+                if (path !== "") {
+                    var parts = path.split(/\./);
+                    while (parts.length > 0) {
+                        var name = parts.shift();
+                        var childIndex = findChildIndex(node, name);
+                        if (childIndex === -1) {
+                            return null;
+                        }
+                        node = node.children[childIndex];
+                    }
                 }
-                return name ? name : "ROOT";
-            })
-            .style("fill", "#000000");
-    };
-};
+                return node;
+            };
+            var findNode = function (path) {
+                var node = findNodeInternal(path);
+                if (node === null) {
+                    throw "Cannot find node " + path;
+                }
+                return node;
+            };
+
+            this.hasNode = function (path) {
+                return findNodeInternal(path) !== null;
+            }
+
+            // Add and remove elements on the graph object
+            this.addNode = function(path) {
+                if (!path) {
+                    return;
+                }
+                var idx = path.lastIndexOf('.');
+                var parentPath = idx === -1 ? "" : path.substring(0, idx);
+                var name = idx === -1 ? path : path.substring(idx + 1);
+                var parent = findNode(parentPath);
+                parent.addChild(name);
+            };
+
+            this.removeNode = function(path) {
+                if (!path) {
+                    return;
+                }
+                var idx = path.lastIndexOf('.');
+                var parentPath = idx === -1 ? "" : path.substring(0, idx);
+                var name = idx === -1 ? path : path.substring(idx + 1);
+                var parent = findNode(parentPath);
+                var childIndex = findChildIndex(parent, name);
+                if (childIndex !== -1) {
+                    parent.children.splice(childIndex, 1);
+                }
+            };
+
+            this.setState = function(path, state) {
+                var node = findNode(path);
+                node.state = state;
+            };
+
+            var getMaxDepth = function (node, currentDepth) {
+                var depth = currentDepth;
+                if (node.children) {
+                    node.children.forEach(function (child) {
+                        depth = Math.max(depth, getMaxDepth(child, currentDepth + 1))
+                    });
+                }
+                return depth;
+            }
+
+            this.repaint = function () {
+                var maxDepth = getMaxDepth(root, 0);
+                tree.size([360, maxDepth * 100]);
+                var svg = canvas;
+                svg.selectAll('.node').remove();
+                svg.selectAll('.link').remove();
+                var nodes = tree.nodes(root);
+                var links = tree.links(nodes);
+
+                var link = svg.selectAll(".link")
+                    .data(links);
+                link
+                    .enter()
+                    .append("path")
+                    .attr("class", "link")
+                    .attr("d", diagonal);
+
+                var node = svg.selectAll(".node")
+                    .data(nodes);
+                var nodeGroup = node
+                    .enter()
+                    .append("g")
+                    .attr("class", "node")
+                    .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+
+                nodeGroup.append("circle")
+                    .attr("r", 6)
+                    .attr("class", function(d) { return d.state; });
+
+                nodeGroup.append("text")
+                    .attr("dy", ".31em")
+                    .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+                    .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+                    .text(function(d) {
+                        var name = d.name;
+                        var idx = name.lastIndexOf(".");
+                        if (idx !== -1) {
+                            name = name.substring(idx + 1);
+                        }
+                        return name ? name : "ROOT";
+                    })
+                    .style("fill", "#000000");
+            };
+        };
+    });
+})();
