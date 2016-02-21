5e9f191d36cb8a7b64044679ef7cf9b8c070932e
Lóránt Pintér <lorant@gradle.com>
2016-02-21 13:44:46 +0100

    Show steps more correctly

--- js/main.js
+++ js/main.js
@@ -24,22 +24,21 @@
 
     app.controller("MainController", function($scope, $timeout, dataLoader, LayoutFactory) {
         $scope.time = 0;
-        $scope.commands = [];
+        $scope.steps = [];
         $scope.playing = false;
         $scope.stepTime = function (delta) {
             var time = parseInt($scope.time) + delta;
-            if (time >= 0 && time <= $scope.commands.length) {
+            if (time >= 0 && time <= $scope.steps.length) {
                 $scope.time = time;
             }
         };
         $scope.states = ["Registered", "Discovered", "Created", "DefaultsApplied", "Initialized", "Mutated", "Finalized", "SelfClosed", "GraphClosed"];
-        $scope.stateCounts = {};
-        $scope.states.forEach(function (state) {
-            $scope.stateCounts[state] = 0;
-        });
-        $scope.count = function() {
+        $scope.count = function(time) {
+            if (time < 0 || time >= $scope.steps.length === 0) {
+                return 0;
+            }
             return $scope.states.reduce(function (count, state) {
-                return count + $scope.stateCounts[state];
+                return count + $scope.steps[time].stateCounts[state];
             }, 0);
         };
 
@@ -54,14 +53,14 @@
 
         dataLoader(dataFile).then(function (events) {
             var layout = new LayoutFactory(svg, d3, width, height);
-            $scope.commands = processEvents(events, layout);
+            $scope.steps = processEvents(events, layout, $scope.states);
             $scope.$watch("time", function (targetTime, time) {
                 redrawUntil(parseInt(targetTime), parseInt(time));
             });
             $scope.$watch("playing", function (playing) {
                 if (playing) {
                     var playLoop = function() {
-                        if ($scope.playing && $scope.time < $scope.commands.length - 1) {
+                        if ($scope.playing && $scope.time < $scope.steps.length - 1) {
                             $scope.time++;
                             $timeout(playLoop, 200);
                         } else {
@@ -76,13 +75,13 @@
             var redrawUntil = function(targetTime, time) {
                 if (time < targetTime) {
                     while (time < targetTime) {
-                        $scope.commands[time].forward($scope.stateCounts);
+                        $scope.steps[time].command.forward();
                         time++;
                     }
                 } else {
                     while (time > targetTime) {
                         time--;
-                        $scope.commands[time].backward($scope.stateCounts);
+                        $scope.steps[time].command.backward();
                     }
                 }
                 layout.repaint();
@@ -90,71 +89,86 @@
         });
     });
 
-    var processEvents = function (events, layout) {
+    var processEvents = function (events, layout, states) {
         var existingNodes = {};
-        var commands = [];
-        var multiproject = Object.size(events.groupBy(function (e) { return e.project; })) > 1;
-        var normalizeEvent = function(event) {
-            if (!multiproject) {
-                return;
+        var steps = [];
+        var projects = Object.keys(events.groupBy(function (e) { return e.project; }));
+        var normalizePath = function(project, path) {
+            if (projects.length === 1) {
+                return path;
             }
-            if (event.project != '') {
-               var path = '<ROOT '+event.project+'>';
-               if (event.path != '') { path += '.' + event.path; }
-               event.path = path;
+            var normalizedPath = '['+ project + ']';
+            if (path !== "") {
+                normalizedPath += '.' + path;
             }
-        }
-        var processEvent = function(event) {
-            normalizeEvent(event);
-            var existingNode = existingNodes[event.path];
-            if (existingNode) {
+            return normalizedPath;
+        };
+        var processEvent = function(event, previousStateCounts) {
+            var path = normalizePath(event.project, event.path);
+            var stateCounts = Object.clone(previousStateCounts);
+            var previousState = existingNodes[path];
+            var command;
+            var description;
+            if (previousState) {
                 var nextState = event.state;
-                var previousState = existingNode.state;
-                commands.push({
-                    description: "'" + event.path + "' now in " + nextState + " (was: " + previousState + ")",
-                    forward: function(stateCounts) {
-                        console.log("-> " + event.path + ": " + previousState + " -> " + nextState);
-                        stateCounts[nextState]++;
-                        stateCounts[previousState]--;
-                        layout.setState(event.path, nextState);
+                if (nextState === previousState) {
+                    return;
+                }
+
+                stateCounts[nextState]++;
+                stateCounts[previousState]--;
+                description = "'" + path + "' now in " + nextState + " (was: " + previousState + ")",
+                command = {
+                    forward: function() {
+                        console.log("-> " + path + ": " + previousState + " -> " + nextState);
+                        layout.setState(path, nextState);
                     },
-                    backward: function(stateCounts) {
-                        console.log("<- " + event.path + ": " + nextState + " -> " + previousState);
-                        stateCounts[nextState]--;
-                        stateCounts[previousState]++;
-                        layout.setState(event.path, previousState);
+                    backward: function() {
+                        console.log("<- " + path + ": " + nextState + " -> " + previousState);
+                        layout.setState(path, previousState);
                     }
-                });
-                existingNode.state = event.state;
+                };
             } else {
-                commands.push({
-                    description: "'" + event.path + "' added  in " + event.state,
-                    forward: function(stateCounts) {
-                        console.log("++ " + event.path);
-                        stateCounts[event.state]++;
-                        layout.addNode(event.path);
+                stateCounts[event.state]++;
+                description = "'" + path + "' added  in " + event.state,
+                command = {
+                    forward: function() {
+                        console.log("++ " + path);
+                        layout.addNode(path);
                     },
-                    backward: function(stateCounts) {
-                        console.log("-- " + event.path);
-                        stateCounts[event.state]--;
-                        layout.removeNode(event.path);
+                    backward: function() {
+                        console.log("-- " + path);
+                        layout.removeNode(path);
                     }
-                });
-                existingNodes[event.path] = {
-                    path: event.path,
-                    state: event.state
                 };
             }
+            existingNodes[path] = event.state;
+            return {
+                description: description,
+                command: command,
+                stateCounts: stateCounts
+            };
         };
-        if (multiproject) {
-        processEvent({ "project": '', "path": '', state: 'Registered'});
+
+        var stateCounts = {};
+        states.forEach(function (state) {
+            stateCounts[state] = 0;
+        });
+
+        projects.forEach(function (project) {
+            var path = normalizePath(project, "");
+            existingNodes[path] = "Registered";
+            stateCounts["Registered"]++;
+            layout.addNode(path);
+        });
+
         events.forEach(function (event) {
-            if (!existingNodes['<ROOT '+event.project+'>']) {
-                  processEvent({ project:'', state: 'Registered', path: '<ROOT '+event.project+'>' });
+            var step = processEvent(event, stateCounts);
+            if (step) {
+                stateCounts = step.stateCounts;
+                steps.push(step);
             }
         });
-        }
-        events.forEach(processEvent);
-        return commands;
+        return steps;
     };
 })();
